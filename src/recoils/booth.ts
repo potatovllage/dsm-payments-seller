58a31fe901eb29c513531fb4178bf22a8050f744
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-23 10:34:35 +0900

    add(recoils) : booth menu

---
+++ src/recoils/booth.ts
@@ -0,0 +1,15 @@
+import { atom } from 'recoil';
+
+import { MenuType } from '../types';
+
+export const menuState = atom<MenuType[]>({
+  key: 'menuState',
+  default: [
+    ...[...Array(20)].map((_, i) => ({
+      boothId: '1',
+      menuId: i,
+      name: `n${i}`,
+      price: (i < 10 ? i * 1000 : -i * 1000) || 1000,
+    })),
+  ],
+});
