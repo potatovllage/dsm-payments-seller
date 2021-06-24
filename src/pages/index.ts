d2fc9898a675e2240190d31969d1fa47a26fa944
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-24 20:01:40 +0900

    update(pages) : barrel file about pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,6 @@
-export { BoothHomePage } from './BoothHomePage';
+export { BoothQRReaderPage } from './BoothQRReader';
+export { BoothHistoryPage } from './BoothHistoryPage';
 export { BoothLoginPage } from './BoothLoginPage';
+export { BoothHomePage } from './BoothHomePage';
 export { BoothPayPage } from './BoothPayPage';
 export { NotFoundPage } from './NotFoundPage';
