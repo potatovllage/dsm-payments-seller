d2842b01c7a65cce133dfca226a4bbb783de3658
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-24 19:58:09 +0900

    update(routers) : BoothRouter

--- src/routers/BoothRouter.tsx
+++ src/routers/BoothRouter.tsx
@@ -2,20 +2,23 @@ import { FC } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { Header, withNavigation, withNeedAuth } from '../components';
-import { NotFoundPage, BoothHomePage, BoothLoginPage, BoothPayPage } from '../pages';
+import { useSocket } from '../hooks/useSocket';
+import { BoothHistoryPage, BoothLoginPage, BoothHomePage, BoothPayPage, BoothQRReaderPage } from '../pages';
 
 const withLogin = (Component: FC) => withNeedAuth(withNavigation(Component));
 
 export const BoothRouter = () => {
+  useSocket();
+
   return (
     <BrowserRouter>
       <Header />
       <Switch>
         <Route path='/booth/login' component={BoothLoginPage} />
+        <Route path='/booth/qr-reader' component={withLogin(BoothQRReaderPage)} />
         <Route path='/booth/payment' component={withLogin(BoothPayPage)} />
-        <Route path='/booth/history' component={withLogin(BoothHomePage)} />
+        <Route path='/booth/history' component={withLogin(BoothHistoryPage)} />
         <Route path='/booth' component={withLogin(BoothHomePage)} />
-        <Route component={NotFoundPage} />
       </Switch>
     </BrowserRouter>
   );
