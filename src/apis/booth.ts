12049b8bdb066cd661ac452263d1725f9e09a975
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-24 20:00:36 +0900

    update(apis) : add apis for booth

--- src/apis/booth.ts
+++ src/apis/booth.ts
@@ -1,18 +1,22 @@
 import axios from 'axios';
-import { MenuType } from '../types';
 
-const BASE_URL = 'http://192.168.137.233:8080';
+import { MenuType, UserType } from '../types';
+
+const BASE_URL = 'https://6a170f85d823.ngrok.io';
+export const SOCKET_URL = 'https://11b9365bd0c0.ngrok.io';
 
 const request = axios.create({
   baseURL: BASE_URL,
 });
 
-const requestWithToken = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-  },
-});
+const requestWithToken = () => {
+  return axios.create({
+    baseURL: BASE_URL,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+    },
+  });
+};
 
 export const postBoothLogin = (id: string, password: string) => {
   return request.post<{ accessToken: string }>('/booth/auth', {
@@ -22,16 +26,33 @@ export const postBoothLogin = (id: string, password: string) => {
 };
 
 export const postBoothMenu = (name: string, price: number) => {
-  return requestWithToken.post<MenuType>('/booth/menu', {
+  return requestWithToken().post<MenuType>('/booth/menu', {
     name,
     price,
   });
 };
 
 export const getBoothMenu = () => {
-  return requestWithToken.get<{ menus: MenuType[] }>('/booth/menu');
+  return requestWithToken().get<{ menus: MenuType[] }>('/booth/menu');
 };
 
 export const deleteBoothMenu = (menuId: number) => {
-  return requestWithToken.delete(`/booth/menu?menuId=${menuId}`);
+  return requestWithToken().delete(`/booth/menu?id=${menuId}`);
+};
+
+export const postPaymentPermission = (userUuid: string) => {
+  return requestWithToken().post('/booth/menu/payment/permission', {
+    userUuid,
+  });
+};
+
+export const getUserInfo = (uuid: string) => {
+  return requestWithToken().get<UserType>(`/user/booth?uuid=${uuid}`);
+};
+
+export const postPayment = (menuId: number, userUuid: string) => {
+  return requestWithToken().post('/booth/menu/payment', {
+    menuId,
+    userUuid,
+  });
 };
