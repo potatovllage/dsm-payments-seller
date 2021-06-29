59a665320bc0a43ccbd211a2dd7a4eab2ef57347
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-29 23:10:06 +0900

    update : apis

--- src/apis/booth.ts
+++ src/apis/booth.ts
@@ -1,21 +1,39 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
-import { MenuType, UserType } from '../types';
+import { BoothInfoType, MenuType, PaymentHistoryType, UserType } from '../types';
 
-const BASE_URL = 'https://6a170f85d823.ngrok.io';
-export const SOCKET_URL = 'https://11b9365bd0c0.ngrok.io';
+const BASE_URL = 'https://5192089a03c0.ngrok.io';
+export const SOCKET_URL = 'https://df4d8611a63c.ngrok.io';
 
 const request = axios.create({
   baseURL: BASE_URL,
 });
 
 const requestWithToken = () => {
-  return axios.create({
+  const instance = axios.create({
     baseURL: BASE_URL,
     headers: {
       Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
     },
   });
+
+  instance.interceptors.response.use(
+    (res) => res,
+    (err: AxiosError) => {
+      const status = err.response?.status;
+
+      if (status === 401 || status === 404) {
+        alert('로그인이 필요합니다.');
+        localStorage.removeItem('accessToken');
+        window.location.href = '/booth/login';
+        return;
+      }
+
+      return Promise.reject(err);
+    }
+  );
+
+  return instance;
 };
 
 export const postBoothLogin = (id: string, password: string) => {
@@ -55,4 +73,12 @@ export const postPayment = (menuId: number, userUuid: string) => {
     menuId,
     userUuid,
   });
+};
+
+export const getBooth = () => {
+  return requestWithToken().get<BoothInfoType>('/booth');
+};
+
+export const getPaymentHistory = (page: number) => {
+  return requestWithToken().get<PaymentHistoryType>(`booth/payment?page=${page}&size=20`);
 };
